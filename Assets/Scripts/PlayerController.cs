0f2d1c5de406bfda043b3cfd86c58e5d3e5949ae
Christian Teguim <148562370+chrisprogod@users.noreply.github.com>
2024-09-13 00:40:45 -0400

    added horizontal movement and horizontale input control

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float horizontalInput;
+    public float turnSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +17,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
         //move the vehicule
         transform.Translate(Vector3.forward *Time.deltaTime*speed);
+        transform.Translate(Vector3.right*Time.deltaTime*turnSpeed);
     }
 }
