32beab49708f8f9e13c2eb30a2e5638d21fc13be
Christian Teguim <148562370+chrisprogod@users.noreply.github.com>
2024-09-12 21:57:29 -0400

    made the camear coordinate public

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    int xCam = 0;
-    int yCam = 5;
-    int zCam = 7;
+    public int xCam = 0;
+    public int yCam = 5;
+    public int zCam = 7;
 
     // Start is called before the first frame update
     void Start()
