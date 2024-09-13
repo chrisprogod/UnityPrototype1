949764e5d0a8e4b48fd90ef4e8ca2fbd80c95fe3
Christian Teguim <148562370+chrisprogod@users.noreply.github.com>
2024-09-13 12:22:31 -0400

    added a restart on R key pressed

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Respawn : MonoBehaviour
 {
@@ -8,9 +10,9 @@ public class Respawn : MonoBehaviour
     
     void FixedUpdate()
     {
-        if(transform.position.y < threshold)
+        if(transform.position.y < threshold || Input.GetKeyDown(KeyCode.R))
         {
-            transform.position = new Vector3(0f,4f, 0f);
+            transform.position = new Vector3(0f,2f, 0f);
             transform.rotation = new Quaternion(0f ,0f,0f, 0f);
         }
     }
